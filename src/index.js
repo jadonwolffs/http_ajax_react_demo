b7a6d0e3b05b6d41a347c7260b24753059e271ad
jadon <jadonwolffs@gmail.com>
2020-01-27 22:05:07 +0200

    added interceptor error handling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 import axios from "axios";
-axios.interceptors.request.use(config=>{
+axios.interceptors.request.use(
+  config => {
     console.log(config);
     //edit
     return config;
-})
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
